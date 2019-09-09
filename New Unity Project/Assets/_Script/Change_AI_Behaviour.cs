de0d9350bd90ed825410f2b3fe6fa4e6e6f2331f
Aidan Robinson <aidanrobinson6@gmail.com>
2019-09-10 11:26:48 +1200

    The_Hambo_ChaserV0.9
    AI Chases player when they get to a certain point

---
+++ New Unity Project/Assets/_Script/Change_AI_Behaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change_AI_Behaviour : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Hampo_Move.instance.follow = true;
+        Debug.Log("trigger is work");
+    }
+
+    /*private void OnCollisionEnter(Collision Platform)
+    {
+        
+    }*/
+}
---
+++ New Unity Project/Assets/_Script/Hampo_Move.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Hampo_Move : MonoBehaviour
+{
+
+    private NavMeshAgent nav;
+    private int currentDestination = 0;
+
+    public List<Transform> target;
+    public float targetDistanceThreshold;
+    public static Hampo_Move instance;
+    public bool follow = false;
+    //these are used to move the AI towards the character
+    public Transform Player;
+    public int MoveSpeed = 4;
+    public int MaxDist = 10;
+    public int MinDist = 5;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(follow == false)
+        {
+         if (nav.remainingDistance < targetDistanceThreshold)
+         {
+          SetNewDestination();
+         }
+        }
+        else if(follow == true)
+        {
+            nav.SetDestination(Player.position);
+            Debug.Log("Bool True");
+        }
+
+    }
+
+    private void SetNewDestination()
+    {
+        currentDestination++;
+
+        if (currentDestination > target.Count - 1)
+        {
+            currentDestination = 0;
+        }
+        nav.SetDestination(target[currentDestination].position);
+    }
+}
